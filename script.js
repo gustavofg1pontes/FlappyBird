487095979a4b7b05c1fb4b6490899137a0d3a3ab
Gustavo <gpontesf06@gmail.com>
2022-12-12 20:15:44 -0300

    inicio decoração

--- script.js
+++ script.js
@@ -2,32 +2,102 @@ function $(element) {
     return document.querySelector(element);
 }
 
-let altura
-let puloALtura = 0
-let velocidadeCaindo = 0
-let bird = $(".flap")
-bird.style.bottom = "38vw"
+let pontos = 0, perdeu = false,
+    posCanos = 30,
+    altura,
+    puloALtura = 0,
+    velocidadeCaindo = 0,
+    velocidadeCanos = -0.02,
+    bird = $(".flap"),
+    pipes = [$(".canoBaixo"), $(".canoCima")];
+
+bird.style.bottom = "38vh"
+
+let contagem = setInterval(() => {
+    pontos++;
+    $(".pontuacao").innerHTML = pontos
+}, 1000);
 
 
 document.addEventListener("keydown", function (e) {
     if (e.key == " ") {
-        puloALtura = .7
+        puloALtura = .6
         velocidadeCaindo = 0
         setTimeout(() => {
             velocidadeCaindo = 0
             puloALtura = 0
-        }, 150);
+        }, 200);
     }
+})
+document.addEventListener("click", function (e) {
+    puloALtura = .6
+    velocidadeCaindo = 0
+    setTimeout(() => {
+        velocidadeCaindo = 0
+        puloALtura = 0
+    }, 200);
 })
 
 function init() {
     requestAnimationFrame(init)
+    if (perdeu) {
+        $(".derrota").style.display = "block";
+        clearInterval(contagem)
+        return
+    }
 
     altura = parseFloat(bird.style.bottom)
     altura -= velocidadeCaindo
     velocidadeCaindo += 0.02
+    posCanos += velocidadeCanos
+    if (velocidadeCanos >= -.5) velocidadeCanos -= 0.0002
+    pipes.forEach(pipe => {
+        pipe.style.left = `${posCanos}vw`
+    })
     bird.style.bottom = `${altura + puloALtura}vw`
-    if (parseInt(bird.style.bottom) < 0) console.log("perdeu")
+    checkColision()
+    randomPipes()
+    if (parseInt(bird.style.bottom) < 0) perdeu = true
+}
+
+function checkColision() {
+    pipes.forEach(pipe => {
+        let pipeInfo = pipe.getBoundingClientRect();
+        let bir = bird.getBoundingClientRect();
+        if (pipeInfo.bottom > bir.top &&
+            pipeInfo.top < bir.bottom &&
+            pipeInfo.left < bir.right &&
+            pipeInfo.right > bir.left) {
+            perdeu = true
+        }
+    })
+}
+
+function randomPipes() {
+    if (pipes[0].offsetLeft <= -pipes[0].getBoundingClientRect().width) {
+        pipes[0].style.bottom = `${Math.random() * -70}vh`
+        pipes[1].style.bottom = `${parseFloat(pipes[0].style.bottom) + 100}vh`
+        pipes.forEach(pipe => {
+            posCanos = 30
+            pipe.style.left = `30vw`
+        })
+    }
+}
+
+function reset() {
+    pontos = 0;
+    perdeu = false
+    bird.style.bottom = "38vh"
+    pipes.forEach(pipe => {
+        posCanos = 30
+        pipe.style.left = `30vw`
+    })
+    $(".derrota").style.display = "none";
+    $(".pontuacao").innerHTML = 0
+    contagem = setInterval(() => {
+        pontos++;
+        $(".pontuacao").innerHTML = pontos
+    }, 1000);
 }
 
 init()
